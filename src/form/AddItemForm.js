9e22de21c206382b6eb78991f1f1f0dde9d3fa44
Jeremy Passeron <jpasseron@gmail.com>
2020-02-25 15:43:32 +0100

    made icon button to work

--- src/form/AddItemForm.js
+++ src/form/AddItemForm.js
@@ -6,20 +6,32 @@ import AddCircle from '@material-ui/icons/AddCircle';
 class AddItemForm extends React.Component {
 
     constructor(props) {
-        super(props)
+        super(props);
+        this.myTxtField = React.createRef();
+
+        this.state = {
+            fieldValue: ""
+        }
     }
 
     keyPress(e) {
-        if(e.keyCode === 13){
-            this.props.addTask(e.target.value);
+        this.setState({ fieldValue: e.target.value });
+        if (e.keyCode === 13) {
+            this.formSubmitted();
             e.target.value = "";
-         }
+        }
+    }
+
+    formSubmitted() {
+        if (this.state.fieldValue && this.state.fieldValue.trim().length > 0) {
+            this.props.addTask(this.state.fieldValue);
+        }
     }
 
     render() {
         return <div>
-            <TextField label="add nother task..." onKeyDown={this.keyPress.bind(this)}/>
-            <IconButton color="secondary" aria-label="add an alarm">
+            <TextField ref={this.myTxtField} label="add nother task..." onKeyDown={this.keyPress.bind(this)} />
+            <IconButton color="secondary" aria-label="add an alarm" onClick={this.formSubmitted.bind(this)}>
                 <AddCircle />
             </IconButton>
         </div>
